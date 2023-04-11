e0d4dbb4bc8b4b61f973a01d4b3adca926494e6a
Phawit Pornwattanakul <me@helloyeew.dev>
2023-04-12 01:59:18 +0700

    📕 Add pagination to the beatmap list in collection

--- backup/urls.py
+++ backup/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from backup import views
 
 urlpatterns = [
-    path('home', views.home, name='backup_home'),
+    path('', views.home, name='backup_home'),
     path('upload', views.upload, name='backup_upload')
 ]
--- collection/views.py
+++ collection/views.py
@@ -1,7 +1,11 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 from collection.models import Collection, CollectionBeatmap
+
+BEATMAPSET_PER_PAGE = 20
 
 
 @login_required
@@ -25,7 +29,6 @@ def collection_list(request):
 
 @login_required
 def collection_detail(request, collection_id):
-    # TODO: Pagination
     collection = Collection.objects.get(id=collection_id)
     beatmaps = CollectionBeatmap.objects.filter(collection=collection).order_by('beatmap__beatmapset__artist', 'beatmap__beatmapset__title')
     # Group beatmapset by beatmapset_id
@@ -35,7 +38,43 @@ def collection_detail(request, collection_id):
         if beatmapset not in beatmapsets:
             beatmapsets[beatmapset] = []
         beatmapsets[beatmapset].append(collection_beatmap.beatmap)
+    total_beatmapset = len(beatmapsets)
+    try:
+        page_number = int(request.GET.get('page', 1))
+    except ValueError:
+        page_number = 1
+    total_page = total_beatmapset // BEATMAPSET_PER_PAGE + 1
+    if total_beatmapset % BEATMAPSET_PER_PAGE == 0:
+        total_page -= 1
+    if page_number > total_page != 0:
+        # add page querystring to the url
+        return redirect(reverse('collections_detail', kwargs={'collection_id': collection_id}) + f'?page={total_page}')
+    if page_number == 0:
+        return redirect(reverse('collections_detail', kwargs={'collection_id': collection_id}))
+    if page_number < 0:
+        return redirect(reverse('collections_detail', kwargs={'collection_id': collection_id}))
+    # If number of beatmapsets is less than 20, then just show all beatmapsets
+    if total_beatmapset <= BEATMAPSET_PER_PAGE:
+        showing_string = f"Showing all {total_beatmapset} beatmapsets"
+        return render(request, 'collection/detail.html', {
+            'collection': collection,
+            'beatmapsets': beatmapsets,
+            'page_number': 1,
+            'total_page': 1,
+            'page_list': range(1, 2),
+            'showing_string': showing_string
+        })
+    # cut the beatmapsets into pages
+    # Slide the beatmapsets from 20*page_number-19 to 20*page_number
+    beatmapsets = list(beatmapsets.items())[BEATMAPSET_PER_PAGE * page_number - BEATMAPSET_PER_PAGE:BEATMAPSET_PER_PAGE * page_number]
+    # Convert back to the same format as before
+    beatmapsets = dict(beatmapsets)
+    showing_string = f"Showing beatmapsets {BEATMAPSET_PER_PAGE * int(page_number) - BEATMAPSET_PER_PAGE + 1} to {BEATMAPSET_PER_PAGE * int(page_number) if BEATMAPSET_PER_PAGE * int(page_number) <= total_beatmapset else total_beatmapset} of {total_beatmapset}"
     return render(request, 'collection/detail.html', {
         'collection': collection,
-        'beatmapsets': beatmapsets
+        'beatmapsets': beatmapsets,
+        'page_number': page_number if page_number <= total_page else total_page,
+        'total_page': total_page,
+        'page_list': range(1, total_page + 1),
+        'showing_string': showing_string
     })
